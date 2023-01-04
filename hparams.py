1da6c836a54a980d482befb60e0c9106b70d2114
Tharuka Kasthuriarachchi <tharuka.ckasthuri@gmail.com>
2023-01-04 13:15:33 +0530

    updated preprocessing and hparams

--- hparams.py
+++ hparams.py
@@ -1,49 +1,53 @@
-import tensorflow as tf
+from collections import namedtuple
+
+
+def get_hparams(**kwargs):
+    return kwargs
 
 
 # hyperparameters:
-hparams = tf.contrib.training.HParams(
-  
-  cleaners='sinhala_cleaners', # setting up sinhala preprocessing steps
+hparams = get_hparams(
 
-  # Audio:
-  num_mels=80, #  Number of Mel banks to generate
-  num_freq=1025, 
-  sample_rate=20000,
-  frame_length_ms=50, #frame length in mili seconds
-  frame_shift_ms=12.5, #frame shift in mili seconds
-  preemphasis=0.97,
+    cleaners='sinhala_cleaners',  # setting up sinhala preprocessing steps
 
-  min_level_db=-100, # factor to normalize and denormalize spectrogram
-  ref_level_db=20,  # 
+    # Audio:
+    num_mels=80,  # Number of Mel banks to generate
+    num_freq=1025,
+    sample_rate=20000,
+    frame_length_ms=50,  # frame length in mili seconds
+    frame_shift_ms=12.5,  # frame shift in mili seconds
+    preemphasis=0.97,
 
-  # Model:
-  outputs_per_step=5, #Reduction factor. Paper => 2, 3, 5
-  embed_depth=256, # Size of character embeddings
-  prenet_depths=[256, 128], #encoder and decoder prenet
-  encoder_depth=256, 
-  postnet_depth=256,
-  attention_depth=256,
-  decoder_depth=256,
+    min_level_db=-100,  # factor to normalize and denormalize spectrogram
+    ref_level_db=20,  #
 
-  # Training:
-  batch_size=32, 
-  adam_beta1=0.9,
-  adam_beta2=0.999,
-  initial_learning_rate=0.002,
-  decay_learning_rate=True,
- 
-  # during evaluation and traing the maximum audio length is equals to
-  # max_iters * outputs_per_step * frame_shift_ms
+    # Model:
+    outputs_per_step=5,  # Reduction factor. Paper => 2, 3, 5
+    embed_depth=256,  # Size of character embeddings
+    prenet_depths=[256, 128],  # encoder and decoder prenet
+    encoder_depth=256,
+    postnet_depth=256,
+    attention_depth=256,
+    decoder_depth=256,
 
-  # Eval:
-  max_iters=800, #Number of maximum iterations
-  griffin_lim_iters=60, #iterations of griffin lim algorithm
-  power=1.5,   # Power to raise magnitudes to prior to Griffin-Lim
+    # Training:
+    batch_size=32,
+    adam_beta1=0.9,
+    adam_beta2=0.999,
+    initial_learning_rate=0.002,
+    decay_learning_rate=True,
+
+    # during evaluation and traing the maximum audio length is equals to
+    # max_iters * outputs_per_step * frame_shift_ms
+
+    # Eval:
+    max_iters=800,  # Number of maximum iterations
+    griffin_lim_iters=60,  # iterations of griffin lim algorithm
+    power=1.5,   # Power to raise magnitudes to prior to Griffin-Lim
 )
 
 
 def hparams_debug_string():
-  values = hparams.values()
-  hp = ['  %s: %s' % (name, values[name]) for name in sorted(values)]
-  return 'Hyperparameters:\n' + '\n'.join(hp)
+    values = hparams
+    hp = ['  %s: %s' % (name, values[name]) for name in sorted(values)]
+    return 'Hyperparameters:\n' + '\n'.join(hp)
